2c12fd3dbdd9bd88e6eeedfbd0082d68594a767d
akrammotory <93082320+akrammotory@users.noreply.github.com>
2025-06-03 10:34:08 +0300

    leason 2

--- SwiftfullThinkingAdvancedLearning/ButtonStyleBootcamp.swift
+++ SwiftfullThinkingAdvancedLearning/ButtonStyleBootcamp.swift
@@ -7,9 +7,39 @@
 
 import SwiftUI
 
+struct PressableStyle: ButtonStyle {
+    
+    let scaledAmount: CGFloat
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? scaledAmount : 1.0)
+//            .brightness(configuration.isPressed ? 0.05: 0)
+            .opacity(configuration.isPressed ? 0.9 : 1.0)
+    }
+    
+}
+
+extension View {
+    
+    func withPressableStyle(scaledAmount: CGFloat = 0.95) -> some View {
+        buttonStyle(PressableStyle(scaledAmount: scaledAmount))
+    }
+    
+}
+
+
 struct ButtonStyleBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            
+        }, label: {
+            Text("Click Me")
+                .font(.headline)
+                .withDefaultButtonFormating()
+        })
+        .withPressableStyle()
+        .padding(40)
     }
 }
 
