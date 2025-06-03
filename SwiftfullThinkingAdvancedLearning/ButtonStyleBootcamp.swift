81f8ebfa263d019ba9af9f4ad2c1452b801fdc51
akrammotory <93082320+akrammotory@users.noreply.github.com>
2025-06-03 10:32:39 +0300

    leason 2

---
+++ SwiftfullThinkingAdvancedLearning/ButtonStyleBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonStyleBootcamp.swift
+//  SwiftfullThinkingAdvancedLearning
+//
+//  Created by Akram Husseini on 03/06/2025.
+//
+
+import SwiftUI
+
+struct ButtonStyleBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonStyleBootcamp()
+}
